b12e2eaf67db957ead65a56c6dbe26fe53afe6e5
bugrayuksel1 <oguzhanbugrayuksel@gmail.com>
2025-01-24 21:05:44 +0300

    router yapısı işlendi

--- express/index.js
+++ express/index.js
@@ -2,20 +2,23 @@ const express = require("express");
 
 const app = express();
 
-// Middleware
-app.use(function (req, res, next) {
-  console.log("Middleware 1");
-  next();
+// en özel olan route en yukarıda olmalı, yoksa gitmiyor orda kalıyor.
+
+app.use("/blogs/:id/users/:username", function (req, res) {
+  console.log(req.params);
+  res.send("blog detay sayfası");
 });
 
-app.use(function (req, res, next) {
-  console.log("Middleware 2");
-  next();
+app.use("/blogs/:id", function (req, res) {
+  res.send("blog detay sayfası");
 });
 
-app.use(function (req, res, next) {
-  console.log("Middleware 3");
-  res.send("Hello World");
+app.use("/blogs", function (req, res) {
+  res.send("blog listesi");
+});
+
+app.use("/", function (req, res) {
+  res.send("anasayfa");
 });
 
 app.listen(3210, function () {
