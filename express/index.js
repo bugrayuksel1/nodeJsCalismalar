e037f51aff5226836da237916e1dd17319e980c8
bugrayuksel1 <oguzhanbugrayuksel@gmail.com>
2025-01-25 14:34:02 +0300

    express router

--- express/index.js
+++ express/index.js
@@ -1,28 +1,15 @@
 const express = require("express");
+const path = require("path");
+const userRoutes = require("./routes/user");
+const adminRoutes = require("./routes/admin");
 
 const app = express();
 
-const path = require("path");
-
 app.use("/libs", express.static("node_modules"));
-//app.use("/libs", express.static(path.join(__dirname, "node_modules"))); böyle de kullanabilrsin.
-
 app.use("/static", express.static("public"));
 
-app.use("/blogs/:id", function (req, res) {
-  console.log(__dirname);
-  console.log(__filename);
-
-  res.sendFile(path.join(__dirname, "views/users", "blog-details.html"));
-});
-
-app.use("/blogs", function (req, res) {
-  res.sendFile(path.join(__dirname, "views/users", "blogs.html"));
-});
-
-app.use("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "views/users", "index.html"));
-});
+app.use(adminRoutes);
+app.use(userRoutes);
 
 app.listen(3210, function () {
   console.log("Server is running on port 3210");
---
+++ express/routes/admin.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+router.use("/admin/blog/create", function (req, res) {
+  res.sendFile(path.join(__dirname, "../views/admin", "blog-create.html"));
+});
+
+router.use("/admin/blogs/:id", function (req, res) {
+  res.sendFile(path.join(__dirname, "../views/admin", "blog-edit.html"));
+});
+
+router.use("/admin/blogs", function (req, res) {
+  res.sendFile(path.join(__dirname, "../views/admin", "blog-list.html"));
+});
+
+router.use("/admin/blogs/:id", function (req, res) {
+  res.sendFile(path.join(__dirname, "../views/admin", "blog-edit.html"));
+});
+
+module.exports = router;
